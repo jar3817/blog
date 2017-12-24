3b855f0a4bd3be1978c560858631d32aa535f293
Joe Reid <joe@joereid.com>
2017-12-24 15:29:00 -0500

    top and bottom

---
+++ _inc/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title><?=$site->settings->site_name?></title>
+		
+		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/assets/css/main.css" rel="stylesheet">
+		<link href="/assets/css/jquery-ui-1.12.1.min.css" rel="stylesheet">
+		<link href="/assets/css/summernote.css" rel="stylesheet">
+		<link href="/assets/css/toastr.min.css" rel="stylesheet">
+		<link href="/assets/css/magicsuggest-min.2.1.4.css" rel="stylesheet">
+			
+		<script src="/assets/js/jquery-3.2.1.min.js"></script>
+		<script src="/assets/js/jquery-ui-1.12.1.min.js"></script>
+		<script src="/assets/js/bootstrap-3.3.7.min.js"></script>
+		<script src="/assets/js/summernote.min.js"></script>
+		<script src="/assets/js/toastr.min.js"></script>
+		<script src="/assets/js/bootstrap-datepicker.min.js"></script>
+		<script src="/assets/js/magicsuggest.2.1.4.js"></script>
+		
+	</head>
+
+<body>
